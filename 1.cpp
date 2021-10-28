f56f9d5f2f41e193761f5633b6e380b226c7d3d2
SofiyaErbis <92259873+SofiyaErbis@users.noreply.github.com>
2021-10-28 14:47:10 +0500

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	srand(time(0));
+	setlocale(LC_ALL, "ru");
+	cout << "Компьютер задумал число n в диапазоне от 1 до 100" << endl;
+	int x = 0 ; 
+	int i = 0;
+	const int n = rand() % 100;
+	const int p = 7;
+	while (i < p && x != n) 
+	{
+		cout << "Попытка " << i + 1  << endl;
+		cin >> x;
+
+		if (x > n)
+			cout << "Загаданное число меньше" << " " << x << endl;
+		if (x < n)
+			cout << "Загаданное число больше" << " " << x << endl;
+		i++;
+	}
+	if (x == n)
+		cout << "Вы угадали число " << n << endl;
+	else
+		cout << "Вы не смогли угадать число" << endl;
+	return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "ru");
+	cout << "Введите число в диапазоне от 100 до 999: " ;
+	int x;
+	cin >> x;
+	int a = x / 100;
+	int z = x % 100;
+	int b = z / 10;
+	int s = x % 10;
+	int c = s % 10;
+
+	if ((a != b) && (a != c) && (b != c))
+	{
+		{
+			cout << a << b << c << endl;
+			cout << a << c << b << endl;
+			cout << b << a << c << endl;
+			cout << b << c << a << endl;
+			cout << c << a << b << endl;
+			cout << c << b << a << endl;
+		}
+
+		if ((a > b) && (a > c)) 
+		{
+			if (b > c)  
+				cout << "Максимальная комбинация:  " << a << b << c << endl; 
+			else
+				cout << "Максимальная комбинация: " << a << c << b << endl;	
+		}
+		if ((b > a) && (b > c)) 
+		{
+			if (a > c)  
+				cout << "Максимальная комбинация: " << b << a << c << endl; 
+			else
+				cout << "Максимальная комбинация: " << b << c << a << endl;
+		}
+		if ((c > b) && (c > a)) 
+		{
+			if (a > b)  
+				cout << "Максимальная комбинация: " << c << a << b << endl; 
+			else
+				cout << "Максимальная комбинация: " << c << b << a << endl;
+		}
+	}
+	else  cout << "Число не удовлетворяет условию"; 
+	return 0;
+}
