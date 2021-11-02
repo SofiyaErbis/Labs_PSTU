4c9631258af193b631b35c50e742228e4a9e90a0
София Эрбис <92259873+SofiyaErbis@users.noreply.github.com>
2021-11-02 18:06:55 +0500

    Update 411.cpp

--- 411.cpp
+++ 411.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-	int i = 1;
-	cout << ++i * i++ << endl;
+	int i = 1;  //Целочисленной  переменной присваивается значение 1
+	cout << ++i * i++ << endl; //Вывод на экран значения выражения
 	return 0;
 }
