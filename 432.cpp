2444348bb94c48c725d4d302b397bf575a8399ec
София Эрбис <92259873+SofiyaErbis@users.noreply.github.com>
2021-11-02 18:32:00 +0500

    Update 432.cpp

--- 432.cpp
+++ 432.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	char a, b, c, d, e;
+	char a, b, c, d, e;//Инициализация переменных под вводимые символы
 	cout << "Введите символы:" << endl;
-	cin >> a >> b >> c >> d >> e;
+	cin >> a >> b >> c >> d >> e;//Ввод символов
 	cout << "Обратная последовательнсоть:" << endl;
 	cout << e << endl;
 	cout << d << endl;
-	cout << c << endl;
+	cout << c << endl; //Вывод обратной последовательности
 	cout << b << endl;
 	cout << a << endl;
 	return 0;
